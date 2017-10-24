6d5e3d86e9463093f3cfe24764c3ae60e7fa3ed1
Baruch <baruchadi@gmail.com>
2017-10-23 23:15:56 -0400

    upload to github

--- index.js
+++ index.js
@@ -1,56 +1,57 @@
 var express = require("express");
-var app = express.createServer(express.logger());
-var io = require("socket.io").listen(app);
-// var io = socket.listen(server);
-app.use(express.static(__dirname+"/public"));
 
-// app.use(express.static('public'))
-// // Create an http server with Node's HTTP module.
-// // Pass it the Express application, and listen on port 8080.
-// var server = require('http').createServer(app);
+// Create a new Express application
+var app = express();
 
-// server.listen(process.env.PORT || 3000);
-// // Instantiate Socket.IO hand have it listen on the Express/HTTP server
-// var socket = require("socket.io");
-// var io = socket.listen(server);
-
-io.configure(function(){
-    io.set("transports",["xhr-polling"]);
-    io.set("polling duration",10);
-})
+app.use(express.static("public"));
+// Create an http server with Node's HTTP module.
+// Pass it the Express application, and listen on port 8080.
+var server = app.listen(process.env.PORT || 3000);
 var port = process.env.PORT || 3000;
-app.listen(port,function(){
-    
-    console.log(`server started, socket open at ${port} in ${app.settings.env} mode...`);
-    
-})
+// Instantiate Socket.IO hand have it listen on the Express/HTTP server
+var socket = require("socket.io");
+var io = socket(server);
+
+var clients = {};
+
+console.log(`server started, socket open at ${port}...`);
 
 io.sockets.on("connection", newConnection);
 
 function newConnection(socket) {
   console.log("new connection: " + socket.id);
+  
+
 
   socket.on("move", moveDog);
   socket.on("connected", sendNewDog);
   socket.on("stay", sitDog);
 
+  socket.on("disconnect", function() {
+    console.log("Got disconnect!");
+
+    var i = allClients.indexOf(socket);
+    allClients.splice(i, 1);
+  });
+
   function sendNewDog(data) {
     console.log("DogJoined");
     data.id = socket.id;
+    allClients[socket.id] = data;
     socket.broadcast.emit("dogJoined", data);
   }
 
   function moveDog(data) {
-      
     console.log("dog is moving");
     data.id = socket.id;
+    allClients[socket.id] = data;
     socket.broadcast.emit("dogMoved", data);
   }
 
   function sitDog(data) {
-      
     console.log("Dog is sitting.");
     data.id = socket.id;
+    allClients[socket.id] = data;
     socket.broadcast.emit("sitDog", data);
   }
 }
